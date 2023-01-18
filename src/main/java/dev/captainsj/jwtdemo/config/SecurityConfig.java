fef021d7c088150a77df63214b704851848da309
CaptainSJ <i.saurabhjoshi@hotmail.com>
2023-01-18 13:40:45 +0530

    Spring Security with JWTs

---
+++ src/main/java/dev/captainsj/jwtdemo/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package dev.captainsj.jwtdemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        return  new InMemoryUserDetailsManager(
+                User.withUsername("captainsj")
+                        .password("{noop}password")
+                        .authorities("read")
+                        .build()
+
+        );
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeRequests(auth -> auth
+                        .anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(withDefaults())
+                .build();
+    }
+}
