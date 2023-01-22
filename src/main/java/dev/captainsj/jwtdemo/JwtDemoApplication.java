21feb0274eabe3ba3952985ea1e1a7873891b4f0
CaptainSJ <i.saurabhjoshi@hotmail.com>
2023-01-23 01:38:13 +0530

    added pem keys for jwt decoder and encoder

--- src/main/java/dev/captainsj/jwtdemo/JwtDemoApplication.java
+++ src/main/java/dev/captainsj/jwtdemo/JwtDemoApplication.java
@@ -1,9 +1,12 @@
 package dev.captainsj.jwtdemo;
 
+import dev.captainsj.jwtdemo.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RsaKeyProperties.class)
 public class JwtDemoApplication {
 
     public static void main(String[] args) {
--- src/main/java/dev/captainsj/jwtdemo/config/SecurityConfig.java
+++ src/main/java/dev/captainsj/jwtdemo/config/SecurityConfig.java
@@ -4,8 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -14,6 +17,13 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final RsaKeyProperties keyProperties;
+
+    public SecurityConfig(RsaKeyProperties keyProperties) {
+        this.keyProperties = keyProperties;
+    }
+
 
     @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
@@ -26,14 +36,22 @@ public class SecurityConfig {
         );
     }
 
+
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeRequests(auth -> auth
                         .anyRequest().authenticated())
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .httpBasic(withDefaults())
                 .build();
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(keyProperties.publicKey()).build();
     }
 }
